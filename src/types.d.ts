2723c4ac398ab59265d813c2a8bbf7f810331ac7
Xiaochuan Ma <mxcins@gmail.com>
2019-09-27 13:35:08 +0800

    config type define

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,3 +3,30 @@
  * multiple
  * components
  */
+export interface IConfig {
+  esm?: IESM | ESM;
+  cjs?: ICJS | CJS;
+  umd?: IUMD | UMD;
+
+  runtimeHelpers?: boolean;
+
+  // for rollup
+  entry?: string;
+  exports: "default" | "named" | "none" | "auto";
+}
+
+interface IESM {
+  type: ESM;
+}
+interface ICJS {
+  type: CJS;
+}
+interface IUMD {
+  type: UMD;
+}
+type ESM = S | M | C;
+type CJS = S | M;
+type UMD = S;
+type S = "single";
+type M = "multiple";
+type C = "components";
